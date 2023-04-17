40be99a3bf1f197649b8e6566878933f5697c1d1
rakesh050791 <rakesh050791@gmail.com>
2023-04-17 23:49:26 +0200

    Day-123 | Homework | Question 1 | class-client-display-method----method-overloading-and-overriding-base

---
+++ class-client-display-method----method-overloading-and-overriding-base/README.md
@@ -0,0 +1,3 @@
+# class-client-display-method----method-overloading-and-overriding-base
+
+1. Complete the code in Client class
---
+++ class-client-display-method----method-overloading-and-overriding-base/src/main/java/Client.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class Client {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+
+        // Make a call to display method as per question requirements here
+
+    }
+
+    // Write the display method as per requirements of question here
+
+}
