c13bd714ca657935b4491200114f956161fe1f7e
rakesh050791 <rakesh050791@gmail.com>
2023-04-28 01:10:38 +0200

    Day-123 | Assignment | Question 1 | create-a-class-point-and-threedpoint---method-overloading-and-overriding-base

--- create-a-class-point-and-threedpoint---method-overloading-and-overriding-base/src/main/java/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        Point p1 = new Point();
-        ThreedPoint t1 = new ThreedPoint();
-        t1.display();
-        p1.display();
-    }
-}
