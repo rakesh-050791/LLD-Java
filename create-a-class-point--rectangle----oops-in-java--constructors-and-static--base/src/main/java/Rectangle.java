608d3e68486cd825446d162027d6443522f712ff
rakesh050791 <rakesh050791@gmail.com>
2023-04-29 00:47:27 +0200

    Day-122 | Assignment Question 2 | Create a class point, rectangle - OOPs in Java (Constructors and Static) | Solution improved

--- create-a-class-point--rectangle----oops-in-java--constructors-and-static--base/src/main/java/Rectangle.java
+++ create-a-class-point--rectangle----oops-in-java--constructors-and-static--base/src/main/java/Rectangle.java
@@ -1,19 +1,26 @@
 public class Rectangle {
-    Point topLeft;
-    Point bottomRight;
+    protected Point topLeft;
+    protected Point bottomRight;
 
      public Rectangle(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY){
-
+         this.topLeft = new Point(topLeftX, topLeftY);
+         this.bottomRight = new Point(bottomRightX, bottomRightY);
     }
 
      public Rectangle(Point topLeft, Point bottomRight){
-        this.topLeft = topLeft;
-        this.bottomRight = bottomRight;
+//        this.topLeft = topLeft;
+//        this.bottomRight = bottomRight;
+
+         this.topLeft = new Point(topLeft);
+         this.bottomRight = new Point(bottomRight);
     }
 
      public Rectangle(Rectangle rectangle){
-        this.topLeft = rectangle.topLeft;
-        this.bottomRight = rectangle.bottomRight;
+//        this.topLeft = rectangle.topLeft;
+//        this.bottomRight = rectangle.bottomRight;
+
+         this.topLeft = new Point(rectangle.topLeft);
+         this.bottomRight = new Point(rectangle.bottomRight);
     }
 
 }
